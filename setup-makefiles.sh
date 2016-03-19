a37c89cd3d3b75ce16340301f6f11dc84e4a7693
Sultan Qasim Khan <sultanqasim@gmail.com>
2016-03-18 20:20:23 -0400

    msm8916-common: remove qcom location stack
    Change-Id: Ie7f0fccc92967b408d52c0b61078942326165452
    
    Conflicts:
    	overlay/frameworks/base/core/res/res/values/config.xml
    	proprietary-files.txt
    	setup-makefiles.sh

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -139,7 +139,6 @@ done
 
 # Pick up overlay for features that depend on non-open-source files
 PRODUCT_PACKAGES += \\
-    com.qualcomm.location \\
     TimeService \\
     qcrilmsgtunnel \\
     qcnvitems \\
@@ -222,17 +221,6 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := \$(COMMON_JAVA_PACKAGE_SUFFIX)
 LOCAL_MODULE_CLASS := JAVA_LIBRARIES
 LOCAL_CERTIFICATE := PRESIGNED
-include \$(BUILD_PREBUILT)
-
-include \$(CLEAR_VARS)
-LOCAL_MODULE := com.qualcomm.location
-LOCAL_MODULE_OWNER := $VENDOR
-LOCAL_SRC_FILES := proprietary/priv-app/\$(LOCAL_MODULE)/\$(LOCAL_MODULE).apk
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_MODULE_CLASS := APPS
-LOCAL_PRIVILEGED_MODULE := true
-LOCAL_CERTIFICATE := platform
 include \$(BUILD_PREBUILT)
 
 include \$(CLEAR_VARS)
