LineageOS/android_device_motorola_msm8916-common

msm8916-common: Move here Moto 8939 specific PowerHAL
Looks smarter than picking in lux and merlin both the same commit every time.

Change-Id: I5b40926e91db2a10258357e06a81c1d47081992b

power/Android.mk
@@ -18,7 +18,11 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_C_INCLUDES := $(TARGET_POWERHAL_HEADER_PATH)
+ifeq ($(filter osprey surnia,$(TARGET_DEVICE)),)
 LOCAL_SRC_FILES := power.c
+else
+LOCAL_SRC_FILES := power_8939.c
+endif
 LOCAL_SHARED_LIBRARIES := liblog libcutils
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE_TAGS := optional

ishubhamsingh/device_motorola_msm8916-common-mm

power/Android.mk
@@ -18,7 +18,11 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_C_INCLUDES := $(TARGET_POWERHAL_HEADER_PATH)
+ifeq ($(filter lux merlin,$(TARGET_DEVICE)),)
 LOCAL_SRC_FILES := power.c
+else
+LOCAL_SRC_FILES := power_8939.c
+endif
 LOCAL_SHARED_LIBRARIES := liblog libcutils
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE_TAGS := optional